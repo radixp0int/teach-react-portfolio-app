b6032af0bb7277e551c1c1fd8560fa5ec7d0acac
Christian Sifuentes <me@radixpoint.dev>
2022-06-26 20:24:57 -0400

    Adding loop for Skills, and adding new images

--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -9,12 +9,58 @@ import Node from '../../assets/node.png';
 import FireBase from '../../assets/firebase.png';
 import AWS from '../../assets/aws.png';
 import GitHub from '../../assets/github.png';
-import Tailwind from '../../assets/tailwind.png';
+import Photoshop from '../../assets/photoshop.png';
 import Mongo from '../../assets/mongo.png';
+import Angular from '../../assets/angular.png';
+import Spring from '../../assets/spring.png';
 
 const Skills = () => {
     const skillsHeader = 'Skills';
     const skillsSubHeader = "// These are the technologies I've worked with";
+    const skillItems = [
+        {
+            src: Angular,
+            altText: 'Angular icon',
+            text: 'Angular'
+        },
+        {
+            src: Spring,
+            altText: 'Spring icon',
+            text: 'Spring'
+        },
+        {
+            src: JavaScript,
+            altText: 'JavaScript icon',
+            text: 'JavaScript'
+        },
+        {
+            src: ReactImg,
+            altText: 'React icon',
+            text: 'React'
+        },
+        {
+            src: Node,
+            altText: 'HTML icon',
+            text: 'React'
+        },
+        {
+            src: Mongo,
+            altText: 'HTML icon',
+            text: 'React'
+        },
+        {
+            src: GitHub,
+            altText: 'GitHub icon',
+            text: 'GitHub'
+        },
+        {
+            src: Photoshop,
+            altText: 'Photoshop icon',
+            text: 'Photoshop'
+        }
+
+    ]
+
     return (
         <div name='skills' className='app_skills'>
             {/* Container */}
@@ -25,38 +71,45 @@ const Skills = () => {
                 </div>
 
                 <div className='app_skills-grid'>
-                    <div className='app_skills-item'>
-                        <img className='app_skills-item-img' src={HTML} alt="HTML icon"/>
-                        <p className='app_skills-item-text'>HTML</p>
-                    </div>
-                    <div className='app_skills-item'>
-                        <img className='app_skills-item-img' src={CSS} alt="HTML icon"/>
-                        <p className='app_skills-item-text'>CSS</p>
-                    </div>
-                    <div className='app_skills-item'>
-                        <img className='app_skills-item-img' src={JavaScript} alt="HTML icon"/>
-                        <p className='app_skills-item-text'>JAVASCRIPT</p>
-                    </div>
-                    <div className='app_skills-item'>
-                        <img className='app_skills-item-img' src={ReactImg} alt="HTML icon"/>
-                        <p className='app_skills-item-text'>REACT</p>
-                    </div>
-                    <div className='app_skills-item'>
-                        <img className='app_skills-item-img' src={GitHub} alt="HTML icon"/>
-                        <p className='app_skills-item-text'>GITHUB</p>
-                    </div>
-                    <div className='app_skills-item'>
-                        <img className='app_skills-item-img' src={Node} alt="HTML icon"/>
-                        <p className='app_skills-item-text'>NODE JS</p>
-                    </div>
-                    <div className='app_skills-item'>
-                        <img className='app_skills-item-img' src={Mongo} alt="HTML icon"/>
-                        <p className='app_skills-item-text'>MONGO DB</p>
-                    </div>
-                    <div className='app_skills-item'>
-                        <img className='app_skills-item-img' src={AWS} alt="HTML icon"/>
-                        <p className='app_skills-item-text'>AWS</p>
-                    </div>
+                    {skillItems.map((skill) =>
+                        <div className='app_skills-item'>
+                            <img className='app_skills-item-img' src={skill.src} alt={skill.altText}/>
+                            <p className='app_skills-item-text'>{skill.text}</p>
+                        </div>
+                    )}
+                    {/*Old Way*/}
+                    {/*<div className='app_skills-item'>*/}
+                    {/*    <img className='app_skills-item-img' src={HTML} alt="HTML icon"/>*/}
+                    {/*    <p className='app_skills-item-text'>HTML</p>*/}
+                    {/*</div>*/}
+                    {/*<div className='app_skills-item'>*/}
+                    {/*    <img className='app_skills-item-img' src={CSS} alt="HTML icon"/>*/}
+                    {/*    <p className='app_skills-item-text'>CSS</p>*/}
+                    {/*</div>*/}
+                    {/*<div className='app_skills-item'>*/}
+                    {/*    <img className='app_skills-item-img' src={JavaScript} alt="HTML icon"/>*/}
+                    {/*    <p className='app_skills-item-text'>JAVASCRIPT</p>*/}
+                    {/*</div>*/}
+                    {/*<div className='app_skills-item'>*/}
+                    {/*    <img className='app_skills-item-img' src={ReactImg} alt="HTML icon"/>*/}
+                    {/*    <p className='app_skills-item-text'>REACT</p>*/}
+                    {/*</div>*/}
+                    {/*<div className='app_skills-item'>*/}
+                    {/*    <img className='app_skills-item-img' src={GitHub} alt="HTML icon"/>*/}
+                    {/*    <p className='app_skills-item-text'>GITHUB</p>*/}
+                    {/*</div>*/}
+                    {/*<div className='app_skills-item'>*/}
+                    {/*    <img className='app_skills-item-img' src={Node} alt="HTML icon"/>*/}
+                    {/*    <p className='app_skills-item-text'>NODE JS</p>*/}
+                    {/*</div>*/}
+                    {/*<div className='app_skills-item'>*/}
+                    {/*    <img className='app_skills-item-img' src={Mongo} alt="HTML icon"/>*/}
+                    {/*    <p className='app_skills-item-text'>MONGO DB</p>*/}
+                    {/*</div>*/}
+                    {/*<div className='app_skills-item'>*/}
+                    {/*    <img className='app_skills-item-img' src={AWS} alt="HTML icon"/>*/}
+                    {/*    <p className='app_skills-item-text'>AWS</p>*/}
+                    {/*</div>*/}
                 </div>
             </div>
         </div>
