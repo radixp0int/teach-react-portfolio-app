ec77b90454718f8567cd33d7d57e6adc3df3d71e
radixpoint <chris.f.sifuentes@gmail.com>
2022-07-02 12:05:17 -0400

    feat(): adding scss for navbar top class

--- src/container/Navbar/Navbar.jsx
+++ src/container/Navbar/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
     const handleClick = () => setNav(!nav);
 
     return (
-        <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+        <div className='app__navbar'>
             <div className='app__navbar-logo'>
                 <img src={Logo} alt='Logo Image' style={{width: '50px'}}/>
             </div>
