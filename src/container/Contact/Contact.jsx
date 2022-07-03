9df1947ccc8022545068685f54bc1b0c128017d6
Christian Sifuentes <me@radixpoint.dev>
2022-07-02 20:05:35 -0400

    Finished styling for Contact component

--- src/container/Contact/Contact.jsx
+++ src/container/Contact/Contact.jsx
@@ -1,20 +1,45 @@
 import React from 'react'
 
+import './Contact.scss'
+
 const Contact = () => {
-  return (
-    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-        <form method='POST' action="https://getform.io/f/a699a1b2-f225-434e-b317-1fbbde8e006c" className='flex flex-col max-w-[600px] w-full'>
-            <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
-                <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email - myemail@gmail.com</p>
-            </div>
-            <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
-            <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
-            <textarea className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message'></textarea>
-            <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
-        </form>
-    </div>
-  )
+    const contactHeader = 'Contact';
+    const contactSubHeader = "// Submit the form below or shoot me an email myemail@gmail.com";
+    const contactSubmit = "Let's Collaborate";
+
+    return (
+        <div name='contact' className='app__contact'>
+            <form
+                method='POST'
+                action="https://getform.io/f/a699a1b2-f225-434e-b317-1fbbde8e006c"
+                className='app__contact-form'
+            >
+                <div className='pb-8'>
+                    <p className='app__contact-header'>{contactHeader}</p>
+                    <p className='app__contact-sub-header'>{contactSubHeader}</p>
+                </div>
+                <input
+                    className='app__contact-name-input'
+                    type="text"
+                    placeholder='Name'
+                    name='name'
+                />
+                <input
+                    className='app__contact-email-input'
+                    type="email"
+                    placeholder='Email'
+                    name='email'
+                />
+                <textarea
+                    className='app__contact-message-input'
+                    name="message"
+                    rows="10"
+                    placeholder='Message'>
+            </textarea>
+                <button className='app__contact-submit-btn'>{contactSubmit}</button>
+            </form>
+        </div>
+    )
 }
 
 export default Contact
