6a52119c988c772433c39a418a55fabb3a793685
Christian Sifuentes <me@radixpoint.dev>
2022-07-09 23:29:48 -0400

    bugfix(): Replacing Typewriter package

--- src/container/Home/Home.jsx
+++ src/container/Home/Home.jsx
@@ -1,20 +1,63 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import {HiArrowNarrowRight} from 'react-icons/hi';
-import Typewriter from "typewriter-effect";
 
 import './Home.scss'
 
 const Home = () => {
     const greeting = 'Hi, my name is';
     const name = 'Nick Haroshka';
-    const statement = "I'm a ";
+    const statements = ["Back End Developer.", "Front End Developer."];
     const description =
         'I’m a full-stack developer specializing in building digital experiences for a Financial Institution. ' +
         'Currently, I’m focused on building responsive full-stack web applications.';
     const nextSection = 'View Work';
 
+    const [index, setIndex] = useState(0);
+    const [subIndex, setSubIndex] = useState(0);
+    const [blink, setBlink] = useState(true);
+
+    const [reverse, setReverse] = useState(false);
+    // typeWriter
+    useEffect(() => {
+        if (index === statements.length - 1 && subIndex === statements[index].length) {
+            return;
+        }
+
+        if (
+            subIndex === statements[index].length + 1 &&
+            index !== statements.length - 1 &&
+            !reverse
+        ) {
+            setReverse(true);
+            return;
+        }
+
+        if (subIndex === 0 && reverse) {
+            setReverse(false);
+            setIndex((prev) => prev + 1);
+            return;
+        }
+
+        const timeout = setTimeout(() => {
+            setSubIndex((prev) => prev + (reverse ? -1 : 1));
+        }, Math.max(reverse ? 75 : subIndex === statements[index].length ? 1000 :
+            150, parseInt(Math.random() * 350)));
+
+        return () => clearTimeout(timeout);
+    }, [subIndex, index, reverse]);
+    // Blinker
+
+
+
+    useEffect(() => {
+        const timeout2 = setTimeout(() => {
+            setBlink((prev) => !prev);
+        }, 500);
+        return () => clearTimeout(timeout2);
+    }, [blink]);
+
     return (
-        <div name='home' className='app__home'>
+        <div id='home' className='app__home'>
             {/* Container */}
             <div className='app__home-container'>
                 <p className='app__home-hello'>
@@ -22,18 +65,7 @@ const Home = () => {
                 <h1 className='app__home-name'>
                     {name}</h1>
                 <h2 className='app__home-statement'>
-                    <Typewriter
-                        options={{
-                            strings: [
-                                `${statement}Back End Developer`,
-                                `${statement}Front End Developer`,
-                                `${statement}UI/UX Designer`],
-                            autoStart: true,
-                            loop: true,
-                            delay: 'natural',
-                            pauseFor: 1100,
-                        }}
-                    />
+                    A {`${statements[index].substring(0, subIndex)}${blink ? "|" : " "}`}
                 </h2>
                 <p className='app__home-description'>
                     {description}</p>
