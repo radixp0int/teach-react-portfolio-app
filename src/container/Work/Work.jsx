e4efe264feaa93ab8afc02443a763adb7b418b4f
Christian Sifuentes <me@radixpoint.dev>
2022-08-10 01:02:28 -0400

    Updating netlify CLI and reducing bad example

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -36,14 +36,12 @@ const Work = () => {
                                     </span>
                                     <div className='app__work-grid-item-btn-container'>
                                         <a href={workItem.button1Href}>
-                                            <button
-                                                className='app__work-grid-item-btn'>
+                                            <button className='app__work-grid-item-btn'>
                                                 {workItem.button1}
                                             </button>
                                         </a>
                                         <a href={workItem.button2Href}>
-                                            <button
-                                                className='app__work-grid-item-btn'>
+                                            <button className='app__work-grid-item-btn'>
                                                 {workItem.button2}
                                             </button>
                                         </a>
@@ -56,134 +54,6 @@ const Work = () => {
                     {/*<div*/}
                     {/*    style={{backgroundImage: `url(${images.RealEstateImg})`}}*/}
                     {/*    className='app__work-grid-item group container content-div'*/}
-                    {/*>*/}
-                    {/*    /!* Hover Effects *!/*/}
-                    {/*    <div className='opacity-0 group-hover:opacity-100'>*/}
-                    {/*        <span className='app__work-grid-item-text'>*/}
-                    {/*            React JS Application*/}
-                    {/*        </span>*/}
-                    {/*        <div className='app__work-grid-item-btn-container'>*/}
-                    {/*            <a href='#'>*/}
-                    {/*                <button*/}
-                    {/*                    className='app__work-grid-item-btn'>*/}
-                    {/*                    Demo*/}
-                    {/*                </button>*/}
-                    {/*            </a>*/}
-                    {/*            <a href='#'>*/}
-                    {/*                <button*/}
-                    {/*                    className='app__work-grid-item-btn'>*/}
-                    {/*                    Code*/}
-                    {/*                </button>*/}
-                    {/*            </a>*/}
-                    {/*        </div>*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
-
-                    {/*<div*/}
-                    {/*    style={{backgroundImage: `url(${images.WorkImg})`}}*/}
-                    {/*    className='app__work-grid-item group container content-div'*/}
-                    {/*>*/}
-                    {/*    /!* Hover Effects *!/*/}
-                    {/*    <div className='opacity-0 group-hover:opacity-100'>*/}
-                    {/*        <span className='app__work-grid-item-text'>*/}
-                    {/*            React JS Application*/}
-                    {/*        </span>*/}
-                    {/*        <div className='app__work-grid-item-btn-container'>*/}
-                    {/*            <a href='#'>*/}
-                    {/*                <button*/}
-                    {/*                    className='app__work-grid-item-btn'>*/}
-                    {/*                    Demo*/}
-                    {/*                </button>*/}
-                    {/*            </a>*/}
-                    {/*            <a href='#'>*/}
-                    {/*                <button*/}
-                    {/*                    className='app__work-grid-item-btn'>*/}
-                    {/*                    Code*/}
-                    {/*                </button>*/}
-                    {/*            </a>*/}
-                    {/*        </div>*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
-                    {/*/!* Grid Item *!/*/}
-                    {/*<div*/}
-                    {/*    style={{backgroundImage: `url(${images.DashboardImg})`}}*/}
-                    {/*    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'*/}
-                    {/*>*/}
-                    {/*    /!* Hover Effects *!/*/}
-                    {/*    <div className='opacity-0 group-hover:opacity-100'>*/}
-                    {/*        <span className='app__work-grid-item-text'>*/}
-                    {/*            React JS Application*/}
-                    {/*        </span>*/}
-                    {/*        <div className='app__work-grid-item-btn-container'>*/}
-                    {/*            <a href='#'>*/}
-                    {/*                <button*/}
-                    {/*                    className='app__work-grid-item-btn'>*/}
-                    {/*                    Demo*/}
-                    {/*                </button>*/}
-                    {/*            </a>*/}
-                    {/*            <a href='#'>*/}
-                    {/*                <button*/}
-                    {/*                    className='app__work-grid-item-btn'>*/}
-                    {/*                    Code*/}
-                    {/*                </button>*/}
-                    {/*            </a>*/}
-                    {/*        </div>*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
-                    {/*<div*/}
-                    {/*    style={{backgroundImage: `url(${images.DashboardImg})`}}*/}
-                    {/*    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'*/}
-                    {/*>*/}
-                    {/*    /!* Hover Effects *!/*/}
-                    {/*    <div className='opacity-0 group-hover:opacity-100'>*/}
-                    {/*        <span className='app__work-grid-item-text'>*/}
-                    {/*            React JS Application*/}
-                    {/*        </span>*/}
-                    {/*        <div className='app__work-grid-item-btn-container'>*/}
-                    {/*            <a href='#'>*/}
-                    {/*                <button*/}
-                    {/*                    className='app__work-grid-item-btn'>*/}
-                    {/*                    Demo*/}
-                    {/*                </button>*/}
-                    {/*            </a>*/}
-                    {/*            <a href='#'>*/}
-                    {/*                <button*/}
-                    {/*                    className='app__work-grid-item-btn'>*/}
-                    {/*                    Code*/}
-                    {/*                </button>*/}
-                    {/*            </a>*/}
-                    {/*        </div>*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
-                    {/*/!* Grid Item *!/*/}
-                    {/*<div*/}
-                    {/*    style={{backgroundImage: `url(${images.RealEstateImg})`}}*/}
-                    {/*    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'*/}
-                    {/*>*/}
-                    {/*    /!* Hover Effects *!/*/}
-                    {/*    <div className='opacity-0 group-hover:opacity-100'>*/}
-                    {/*        <span className='app__work-grid-item-text'>*/}
-                    {/*            React JS Application*/}
-                    {/*        </span>*/}
-                    {/*        <div className='app__work-grid-item-btn-container'>*/}
-                    {/*            <a href='#'>*/}
-                    {/*                <button*/}
-                    {/*                    className='app__work-grid-item-btn'>*/}
-                    {/*                    Demo*/}
-                    {/*                </button>*/}
-                    {/*            </a>*/}
-                    {/*            <a href='#'>*/}
-                    {/*                <button*/}
-                    {/*                    className='app__work-grid-item-btn'>*/}
-                    {/*                    Code*/}
-                    {/*                </button>*/}
-                    {/*            </a>*/}
-                    {/*        </div>*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
-                    {/*<div*/}
-                    {/*    style={{backgroundImage: `url(${images.ExpensifyImg})`}}*/}
-                    {/*    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'*/}
                     {/*>*/}
                     {/*    /!* Hover Effects *!/*/}
                     {/*    <div className='opacity-0 group-hover:opacity-100'>*/}
